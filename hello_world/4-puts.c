71ae4f284e6e66060a3b78950d65c3938730203a
HansSora <hansisoraa@gmail.com>
2024-06-18 08:38:16 +0000

    changes

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - This is the entry point of the code
+ *
+ * Return - 0 Successful
+ */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
