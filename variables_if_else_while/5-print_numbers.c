2fc9de302840015daf140070f0494160ed5fe3d8
HansSora <hansisoraa@gmail.com>
2024-06-18 12:04:40 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; c++)
+{
+printf("%d", c);
+}
+putchar('\n');
+return (0);
+}
