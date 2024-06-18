5a43bf441a217b0102cec7b480bf57095c65ca4b
HansSora <hansisoraa@gmail.com>
2024-06-18 11:59:09 +0000

    changes

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,14 +11,13 @@
 */
 int main(void)
 {
-
 char c;
 
 for (c = 'a'; c <= 'z'; c++)
 {
-if (c != 'q' && c != 'e')
+if ( c != 'q' && c != 'e' )
 {
-    putchar(c);
+putchar(c);
 }
 }
 putchar('\n');
