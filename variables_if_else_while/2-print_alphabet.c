ad30798b81f10a51b87f1ab05a6254107ba096ae
HansSora <hansisoraa@gmail.com>
2024-06-18 11:31:11 +0000

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+for(char c = 'a'; c < 'z'; c ++){
+putchar(c);
+}
+putchar('\n');
+}
