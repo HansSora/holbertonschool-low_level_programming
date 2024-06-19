88b371bc83f7752517856dfe5a948abcfa8d9ee8
HansSora <hansisoraa@gmail.com>
2024-06-19 10:21:48 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,10 +10,9 @@
 *
 *Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
-    const char message[] = "_putchar\n";
-    write(1, message, sizeof(message) - 1); 
-    
-    return (0);
+const char message[] = "_putchar\n";
+write(1, message, sizeof(message) - 1);
+return (0);
 }
