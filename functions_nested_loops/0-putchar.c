f77137f2465ee53f01fcf1fd90af8105fa0ee6ff
HansSora <hansisoraa@gmail.com>
2024-06-19 10:19:24 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <unistd.h>
+
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
@@ -8,11 +10,12 @@
 *
 *Return: Always 0 (Success)
 */
-int main(void)
+int main(void) 
 {
-
-puts("_putchar\n");
-
-return(0);
-
+    const char message[] = "_putchar\n";
+    write(1, message, sizeof(message) - 1); 
+    
+    // sizeof(message) includes the null terminator, so we subtract 1
+    
+    return (0);
 }
