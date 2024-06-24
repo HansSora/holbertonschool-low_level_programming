7700cb99ec9a6ec80685fa41add83106d74d3719
HansSora <hansisoraa@gmail.com>
2024-06-24 11:43:46 +0000

    print_line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print line - Prints a line of underscores in the terminal.
+*@n: Number of times the character '_' should be printed.
+*/
+
+void print_line(int n)
+
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar ('\n');
+}
