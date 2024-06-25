d5cabd29c09ce784e98f62115452721913e81420
HansSora <hansisoraa@gmail.com>
2024-06-25 10:05:37 +0000

    puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,23 +1,15 @@
 #include "main.h"
-/**
-* rev_string - Reverses a string.
-* @s: Pointer to the string to be reversed.
-*/
-void rev_string(char *s)
-{
-int length = 0;
-char *start, *end, temp;
 
-while (*(s + length) != '\0')
-length++;
-start = s;
-end = s + length - 1;
-while (start < end)
+/**
+* _puts - Prints a string followed by a new line to stdout.
+* @str: Pointer to the string to be printed.
+*/
+void _puts(char *str)
 {
-temp = *start;
-*start = *end;
-*end = temp;
-start++;
-end--;
+while (*str != '\0')
+{
+	_putchar (*str);
+	str++;
 }
+_putchar('\n');
 }
