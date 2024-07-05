5296d2439aca719743a6241b60ac3857f3d4e845
HansSora <hansisoraa@gmail.com>
2024-07-05 08:06:41 +0000

    whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: argument count
+* @argv: argument array
+*
+* Return: Always 0.
+*/
+int main(int argc __attribute__((__unused__)), char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
