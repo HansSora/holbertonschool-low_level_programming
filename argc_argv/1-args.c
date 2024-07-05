c98050a63039d2799fbbfe1cc934e2ac63e2c882
HansSora <hansisoraa@gmail.com>
2024-07-05 08:14:02 +0000

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: argument count
+* @argv: argument array
+*
+* Return: Always 0.
+*/
+int main(int argc, __attribute__((__unused__)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
