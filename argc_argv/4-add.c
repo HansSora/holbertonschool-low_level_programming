5267bcd8442ee4424d478f000a144a495854076c
HansSora <hansisoraa@gmail.com>
2024-07-05 08:53:35 +0000

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+* @argc: argument count
+* @argv: argument array
+*
+* Return: Always 0.
+*/
+int main(int argc __attribute__((__unused__)), char *argv[])
+{
+	int i, result;
+
+	result = 0;
+	for (i = 0; i < argc; i++)
+	{
+		result = +atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
