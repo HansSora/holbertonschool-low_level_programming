10075740a16b29abc7e57479048e3c9c27f24de2
HansSora <hansisoraa@gmail.com>
2024-07-16 10:10:17 +0000

    print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - function that prints a name
+ * @name: the name
+ * @f: function
+ */
+ 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
