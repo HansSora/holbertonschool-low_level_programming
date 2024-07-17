e964e859b10b521e34647babf1ffcec2f2778079
HansSora <hansisoraa@gmail.com>
2024-07-17 08:13:44 +0000

    get_op_func

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -23,19 +23,19 @@ int main(int argc, char *argv[])
 	op = argv[2];
 	num2 = atoi(argv[3]);
 	num1 = atoi(argv[1]);
-	
+
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
+
 	if ((*op == '/' || *op == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	
+
 	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
