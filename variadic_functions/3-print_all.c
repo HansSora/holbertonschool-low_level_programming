4d166bf8ad5d997df0d694ae0e5951a542d7338d
HansSora <hansisoraa@gmail.com>
2024-07-19 08:02:53 +0000

    print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,83 +1,80 @@
 #include "variadic_functions.h"
-#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
 
-void pr_char(va_list char_list);
-void pr_int(va_list int_list);
-void pr_float(va_list float_list);
-void pr_string(va_list string_list);
-void print_all(const char * const format, ...);
 /**
- * pr_char - print character
- * @char_list: list of char
- */
+* pr_char - print character
+* @char_list: va_list containing the character to print
+*/
 void pr_char(va_list char_list)
 {
-	printf("%c", va_arg(char_list, int));
+printf("%c", va_arg(char_list, int));
 }
+
 /**
- * pr_int - print intger
- * @int_list: list of int
- */
+* pr_int - print integer
+* @int_list: va_list containing the integer to print
+*/
 void pr_int(va_list int_list)
 {
-	printf("%i", va_arg(int_list, int));
+printf("%i", va_arg(int_list, int));
 }
+
 /**
- * pr_float - print float
- * @float_list: list of float
- */
+* pr_float - print float
+* @float_list: va_list containing the float to print
+*/
 void pr_float(va_list float_list)
 {
-	printf("%f", va_arg(float_list, double));
+printf("%f", va_arg(float_list, double));
 }
+
 /**
- * pr_string - print string
- * @string_list: list of strings
- */
+* pr_string - print string
+* @string_list: va_list containing the string to print
+*/
 void pr_string(va_list string_list)
 {
-	char *s;
+char *s = va_arg(string_list, char*);
 
-	s = va_arg(string_list, char*);
-	if (s == NULL)
-		printf("(nil)");
+if (s == NULL)
+	printf("(nil)");
+else
 	printf("%s", s);
 }
+
 /**
- * print_all - function that prints any format
- * @format: format is a list of arguments passed to the function
- * @...: args
- */
+* print_all - prints anything, based on the format provided
+* @format: format string that defines the types of the following arguments
+* @...: variadic arguments list
+*/
 void print_all(const char * const format, ...)
 {
-	int k, l;
-	va_list ag_list;
-	char *separator = "";
-	p_d p_func[] = {
-		{"c", pr_char},
-		{"s", pr_string},
-		{"f", pr_float},
-		{"i", pr_int}
-	};
-	va_start(ag_list, format);
-	l = 0;
-	while (format != NULL && format[l] != '\0')
+va_list ag_list;
+unsigned int i = 0, j;
+char *separator = "";
+p_d p_func[] = {
+	{"c", pr_char},
+	{"s", pr_string},
+	{"f", pr_float},
+	{"i", pr_int}
+};
+
+va_start(ag_list, format);
+while (format && format[i])
+{
+	j = 0;
+	while (j < 4 && *p_func[j].f_type != format[i])
+		j++;
+
+	if (j < 4)
 	{
-		k = 0;
-		while (k < 4 && format[l] != *p_func[k].f_type)
-			k++;
-
-		if (k < 4)
-		{
-			printf("%s", separator);
-			p_func[k].print(ag_list);
-			separator = ", ";
-		}
-		l++;
-
+		printf("%s", separator);
+		p_func[j].print(ag_list);
+		separator = ", ";
 	}
-	printf("\n");
-	va_end(ag_list);
+	i++;
+}
+printf("\n");
+va_end(ag_list);
 }
